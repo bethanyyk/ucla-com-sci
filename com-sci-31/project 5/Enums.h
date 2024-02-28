cc2faa0d53be99bf2fcfb33c51e806c7a74bdfa0
bethanyyk <147457852+bethanyyk@users.noreply.github.com>
2024-02-28 10:53:13 -0800

    project 5

---
+++ com-sci-31/project 5/Enums.h
@@ -0,0 +1,7 @@
+#ifndef Enums_h
+#define Enums_h
+
+enum meal { BREAKFAST, LUNCH, DINNER, WEEKENDBRUNCH };
+enum plan { UNDEFINED, REGULAR11, PREMIER11, REGULAR14, PREMIER14, REGULAR19, PREMIER19 };
+
+#endif
